2697ed18d327452311f373bbd34acd8a0b318d56
Kamil <172912915+Wyngar@users.noreply.github.com>
2024-07-04 00:35:01 +0000

    Done all the exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    return array.filter((element) => {
+        return !num.includes(element);
+    })
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// Uzywamy filter aby odfiltrowac posczegolne elementy arraya
+// odwracamy dzialanie filter poprzez zastosowanie ! gdzie sprawdzamy czy nowa tablica (...)num (nasze argumenty ktore chcemy wyrzucic) zawieraja elementy (poszczegolne elemenety arraya), jesli nie zawieraja, zwraca true, czyli element zostaje
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    // zapozyczone z solution \/
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0 || isNaN(x) || isNaN(y)) {
+        return "ERROR"
+    } else if (x > y) {
+        for (y;y <= x; y++) {
+            sum += y
+        }
+        return sum;
+    } else if (x < y) {
+        for (x;x <= y; x++) {
+            sum += x
+        }
+        return sum;
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
